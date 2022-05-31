bb44ef10ca71455108ce07c791272092152af3c6
Venkata Puchaginajala <Venkata.Puchaginajala.contractor@translink.ca>
2022-05-31 13:44:22 -0700

    added ResponseExtension

---
+++ TestAutomationFramework/Extensions/ResponseExtension.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace TestAutomationFramework.Extensions
+{
+    public static class ResponseExtension
+    {
+        public static dynamic Extract(this RestResponse response)
+        {
+            dynamic deserializedResponse = JsonConvert.DeserializeObject(response.Content);
+            return deserializedResponse;
+        }
+
+        public static T ExtractAs<T>(this RestResponse response) where T : new()
+        {
+            return JsonConvert.DeserializeObject<T>(response.Content);
+        }
+        public static string Path(this RestResponse response, string jsonPath)
+        {
+            var jObject = JObject.Parse(response.Content);
+            var value = (string)jObject.SelectToken(jsonPath);
+            return value;
+        }
+    }
+}
